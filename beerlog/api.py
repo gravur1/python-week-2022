048067ff5b94dfd6c5bf5dbb69a5acfd2a0a4971
gravur1 <tiagometur@gmail.com>
2022-04-27 07:08:02 -0300

    API is listing and adding beers

---
+++ beerlog/api.py
@@ -0,0 +1,26 @@
+from typing import List
+from fastapi import FastAPI
+from beerlog.core import get_beers_from_database
+from beerlog.serializers import BeerOut, BeerIn
+from beerlog.database import get_session
+from beerlog.models import Beer
+
+api = FastAPI(title="Beerlog") #python -m uvicorn beerlog.api:api --reload
+
+#http://127.0.0.1:8000/redoc
+#http://127.0.0.1:8000/docs
+
+@api.get("/beers/", response_model=List[BeerOut])
+async def list_beers():
+    """Lists beers from the database"""
+    beers = get_beers_from_database()
+    return beers
+
+@api.post("/beers/", response_model=BeerOut)
+async def add_beer(beer_in: BeerIn):
+    beer = Beer(**beer_in.dict())
+    with get_session() as session:
+        session.add(beer)
+        session.commit()
+        session.refresh(beer)    
+    return beer
